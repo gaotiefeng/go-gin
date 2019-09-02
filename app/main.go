57e453fff0540091eb524031d552b77e0a667458
高铁峰 <1096392101@qq.com>
2019-09-02 17:46:35 +0800

    testing

--- app/main.go
+++ app/main.go
@@ -7,6 +7,5 @@ import (
 func main() {
 
 	router := router.SetupRouter()
-
 	_ = router.Run()
 }
--- app/router/api.go
+++ app/router/api.go
@@ -14,11 +14,11 @@ func SetupRouter() *gin.Engine {
 	// 添加 Get 请求路由
 	router.GET("/", controller.Index)
 	router.GET("/welcome", controller.Welcome)
-	v1 := router.Group("/api")
+	v1 := router.Group("/")
 	{
-		v1.POST("/login", new(api.User).Login)
-		v1.POST("/register", new(api.User).Register)
-		v1.GET("/userInfo/:id", new(api.User).UserInfo)
+		v1.POST("/user/login", new(api.User).Login)
+		v1.POST("/user/register", new(api.User).Register)
+		v1.GET("/user/info", new(api.User).UserInfo)
 	}
 	v2 := router.Group("admin")
 	{
